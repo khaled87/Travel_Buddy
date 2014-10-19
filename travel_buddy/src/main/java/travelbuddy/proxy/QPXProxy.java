452f996dfefd6e81ff698d2933903d0b624eefdd
khaled87 <khaled.nawasreh@gmail.com>
2014-10-19 20:04:49 +0200

    Resolve conflicts

--- travel_buddy/src/main/java/travelbuddy/proxy/QPXProxy.java
+++ travel_buddy/src/main/java/travelbuddy/proxy/QPXProxy.java
@@ -2,18 +2,6 @@ package travelbuddy.proxy;
 
 import com.google.gson.Gson;
 import javax.ejb.Stateless;
-<<<<<<< HEAD
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.json.stream.JsonParser;
-import javax.ws.rs.core.Request;
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONValue;
-=======
->>>>>>> d9f857e3bb82cec3a821db9484981a3812f3f9b4
 import travelbuddy.common.BasicQPXRequest;
 import travelbuddy.common.FlightResponse;
 
