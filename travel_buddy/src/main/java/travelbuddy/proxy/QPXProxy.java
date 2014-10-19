d9f857e3bb82cec3a821db9484981a3812f3f9b4
DanielDHuynh <hthduy@gmail.com>
2014-10-19 18:50:59 +0200

    Resolve conflicts

--- travel_buddy/src/main/java/travelbuddy/proxy/QPXProxy.java
+++ travel_buddy/src/main/java/travelbuddy/proxy/QPXProxy.java
@@ -1,42 +1,13 @@
 package travelbuddy.proxy;
 
-<<<<<<< HEAD
 import com.google.gson.Gson;
-=======
-import com.eclipsesource.json.JsonArray;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.ejb.EJB;
->>>>>>> origin/master
 import javax.ejb.Stateless;
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.json.stream.JsonParser;
-import javax.ws.rs.core.Request;
-import jdk.nashorn.internal.parser.JSONParser;
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONValue;
 import travelbuddy.common.BasicQPXRequest;
 import travelbuddy.common.FlightResponse;
-<<<<<<< HEAD
-=======
-import travelbuddy.entity.Flight;
->>>>>>> origin/master
 
 @Stateless
 public class QPXProxy implements IQPXProxy {
 
-    private QPXProxy qPXProxy;
     private final String url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=";
     private final String ApiKey = "AIzaSyBK9KNDV3W5tIc121fI07eRYDGcT3yGUJU";
 
@@ -45,6 +16,5 @@ public class QPXProxy implements IQPXProxy {
         String value = RequestHandler.execute(RequestHandler.RequestMethod.POST, url + ApiKey, fr.toParam());
 
         return new Gson().fromJson(value, FlightResponse.class);
-
     }
 }
